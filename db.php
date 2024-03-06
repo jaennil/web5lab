ac300ecfa1a13468fe0b5d8f1fbd6322f1f77875
root <root@2600427-vo09475.twc1.net>
2024-03-06 17:21:12 +0300

    db creds

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'std-mysql.ist.mospolytech.ru');
+define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
-define('DB_USER', 'std_2276_web5');
-define('DB_PASSWORD', '123456788');
-define('DB_NAME', 'std_2276_web5');
+define('DB_USER', 'jaennil');
+define('DB_PASSWORD', 'naeNN6457!');
+define('DB_NAME', 'web5');
 $mysql = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 ?>
